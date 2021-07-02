fb1c23894e26a17a7b342761bff78c4db1db52f7
KenanMaslesa <kenoo.maslesa@gmail.com>
2021-07-02 15:12:24 +0200

    second way

--- src/app/components/paypal/paypal.component.ts
+++ src/app/components/paypal/paypal.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import {render} from 'creditcardpayments/creditCardPayments';
+
+declare var paypal;
 
 @Component({
   selector: 'app-paypal',
@@ -8,21 +9,42 @@ import {render} from 'creditcardpayments/creditCardPayments';
   styleUrls: ['./paypal.component.scss']
 })
 export class PaypalComponent implements OnInit {
+  @ViewChild('paypal', { static: true }) paypalElement: ElementRef;
+  
+  constructor(public router: Router){}
 
-  constructor(public router: Router){
-    render({
-      id: "#paypalButtons",
-      currency: "USD",
-      value: "1",
-      onApprove: (details) =>{
-        console.log(details);
-        alert(details);
-        this.router.navigate(['successful-payment'])
-      }
-    })
-  }
+  product = {
+    price: 10,
+    description: 'The best course ever',
+    img: 'assets/ng-course.jpeg'
+  };
 
-  ngOnInit(): void {
+  ngOnInit() {
+    paypal
+      .Buttons({
+        createOrder: (data, actions) => {
+          return actions.order.create({
+            purchase_units: [
+              {
+                description: this.product.description,
+                amount: {
+                  currency_code: 'USD',
+                  value: this.product.price
+                }
+              }
+            ]
+          });
+        },
+        onApprove: async (data, actions) => {
+          const order = await actions.order.capture();
+          this.router.navigate(['successful-payment'])
+          console.log(order);
+        },
+        onError: err => {
+          alert(err);
+        }
+      })
+      .render(this.paypalElement.nativeElement);
   }
 
 }
